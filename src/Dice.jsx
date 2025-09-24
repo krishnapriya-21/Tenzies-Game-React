9b449b959a3f179a927a6d615107f510236f9282
krishnapriya Shankar <krithikakrishnapriya@gmail.com>
2025-09-24 17:27:25 +0530

    Dice Component props Update

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,8 +1,17 @@
-export default function dice(){
+export default function Dice(props){
 
-
+    const styles= {
+       backgroundColor:  props.isHeld ? "#59E391" : "white"
+    
+}
     return(
 
-        <button className="dice" >{Math.ceil(Math.random()) }</button>
+        <button 
+        className="dice"
+        style={styles}
+        onClick={()=>props.holdDice(props.id)}
+        >{props.value}</button>
     )
 }
+
+
